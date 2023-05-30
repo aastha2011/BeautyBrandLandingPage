06d16b62cad478a2e18024b553902cc662750b41
aastha2011 <aastha2011>
2023-05-30 09:08:33 +0000

    Pushed from Locofy Builder

---
+++ src/pages/Desktop.tsx
@@ -0,0 +1,270 @@
+import { FunctionComponent } from "react";
+import styles from "./Desktop.module.css";
+const Desktop: FunctionComponent = () => {
+  return (
+    <div className={styles.desktop}>
+      <img className={styles.desktopChild} alt="" src="/group-5.svg" />
+      <div className={styles.growingAndGlowingWithVelveWrapper}>
+        <div className={styles.growingAndGlowing}>
+          Growing and Glowing with Velvet Vanity!
+        </div>
+      </div>
+      <div className={styles.logoParent}>
+        <div className={styles.logo}>
+          <b className={styles.velvetVanity}>Velvet Vanity</b>
+          <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+        </div>
+        <div className={styles.homeParent}>
+          <div className={styles.home}>Home</div>
+          <div className={styles.about}>About</div>
+          <div className={styles.contact}>Contact</div>
+          <div className={styles.shop}>Shop</div>
+        </div>
+      </div>
+      <div className={styles.bookNowWrapper}>
+        <div className={styles.bookNow}>Book Now</div>
+      </div>
+      <div className={styles.shopNowWrapper}>
+        <div className={styles.bookNow}>Shop Now</div>
+      </div>
+      <div className={styles.parent}>
+        <b className={styles.b}>500+</b>
+        <b className={styles.b1}>3000+</b>
+        <b className={styles.k}>10K+</b>
+        <div className={styles.clients}>Clients</div>
+        <div className={styles.brands}>Brands</div>
+        <div className={styles.products}>Products</div>
+        <img className={styles.groupChild} alt="" src="/line-1.svg" />
+        <img className={styles.groupItem} alt="" src="/line-2.svg" />
+      </div>
+      <img
+        className={styles.unsplashaolMvxprmkIcon}
+        alt=""
+        src="/unsplashaolmvxprmk@2x.png"
+      />
+      <div className={styles.rectangleParent}>
+        <div className={styles.groupInner} />
+        <div className={styles.ellipseDiv} />
+        <div className={styles.creativeNailsExpress}>
+          Creative nails express personality.
+        </div>
+        <div className={styles.nails}>Nails</div>
+        <img
+          className={styles.unsplashppwrie5mhdqIcon}
+          alt=""
+          src="/unsplashppwrie5mhdq@2x.png"
+        />
+      </div>
+      <div className={styles.makeupParent}>
+        <div className={styles.makeup}>Makeup</div>
+        <div className={styles.enhanceBeautyWith}>
+          Enhance beauty with cosmetics.
+        </div>
+        <div className={styles.groupInner} />
+        <div className={styles.groupChild1} />
+        <img
+          className={styles.unsplashrgwFUh3mkIcon}
+          alt=""
+          src="/unsplashrgwf-uh3mk@2x.png"
+        />
+      </div>
+      <div className={styles.spaParent}>
+        <div className={styles.makeup}>Spa</div>
+        <div className={styles.enhanceBeautyWith}>
+          Relaxation, rejuvenation, body bliss.
+        </div>
+        <div className={styles.groupInner} />
+        <div className={styles.groupChild1} />
+        <img
+          className={styles.unsplashzbzydbon7fgIcon}
+          alt=""
+          src="/unsplashzbzydbon7fg@2x.png"
+        />
+      </div>
+      <div className={styles.makeupGroup}>
+        <div className={styles.makeup}>Makeup</div>
+        <div className={styles.enhanceBeautyWith}>
+          Revitalize skin with massage.
+        </div>
+        <div className={styles.groupInner} />
+        <div className={styles.groupChild1} />
+      </div>
+      <div className={styles.categories}>Categories</div>
+      <img
+        className={styles.unsplashdsyIlnh69aIcon}
+        alt=""
+        src="/unsplashdsy-ilnh69a@2x.png"
+      />
+      <b className={styles.featured}>Featured</b>
+      <div className={styles.fromNourishingSerums}>
+        From nourishing serums to vibrant lipsticks, our collection offers
+        endless possibilities for self-expression. Immerse yourself in a world
+        of elegance and sophistication as you explore our meticulously curated
+        range.
+      </div>
+      <img
+        className={styles.unsplashscfl6r7loqkIcon}
+        alt=""
+        src="/unsplashscfl6r7loqk@2x.png"
+      />
+      <img
+        className={styles.unsplash42nkyrog7gIcon}
+        alt=""
+        src="/unsplash-42nkyrog7g@2x.png"
+      />
+      <div className={styles.shopAllYourFavoritesWrapper}>
+        <div className={styles.bookNow}>Shop all your favorites</div>
+      </div>
+      <div className={styles.viewAllWrapper}>
+        <div className={styles.bookNow}>
+          <span>{`View All `}</span>
+          <span className={styles.span}>{`->`}</span>
+        </div>
+      </div>
+      <div className={styles.createBeautyMagic}>
+        Create Beauty Magic: Unveil the Perfect Pair
+      </div>
+      <div className={styles.topBrandedProducts}>Top Branded Products</div>
+      <div className={styles.checkoutLatestNews}>Checkout Latest News</div>
+      <div className={styles.featuredParent}>
+        <div className={styles.bookNow}>Featured</div>
+        <div className={styles.bookNow}>Popular</div>
+        <div className={styles.bookNow}>Category</div>
+        <div className={styles.bookNow}>Brand</div>
+      </div>
+      <img className={styles.desktopItem} alt="" src="/line-3.svg" />
+      <img className={styles.desktopInner} alt="" src="/line-4.svg" />
+      <img
+        className={styles.unsplashltgwgq7rMcIcon}
+        alt=""
+        src="/unsplashltgwgq7r-mc@2x.png"
+      />
+      <img
+        className={styles.unsplash4wo5gon3mg4Icon}
+        alt=""
+        src="/unsplash4wo5gon3mg4@2x.png"
+      />
+      <img
+        className={styles.unsplasht8ueo1lnzeuIcon}
+        alt=""
+        src="/unsplasht8ueo1lnzeu@2x.png"
+      />
+      <div className={styles.bodyWash}>Body Wash</div>
+      <div className={styles.cleansingOil}>Cleansing Oil</div>
+      <div className={styles.bodyPerfume}>Body Perfume</div>
+      <div className={styles.div}>$24.5</div>
+      <div className={styles.div1}>$40</div>
+      <div className={styles.div2}>$50</div>
+      <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+      <div className={styles.rectangleGroup}>
+        <div className={styles.groupChild6} />
+        <img
+          className={styles.unsplashxCxdiuhs3cIcon}
+          alt=""
+          src="/unsplashxcxdiuhs3c@2x.png"
+        />
+        <div className={styles.beautyParent}>
+          <div className={styles.bookNow}>{`Beauty `}</div>
+          <img className={styles.vectorIcon2} alt="" src="/vector2.svg" />
+          <div className={styles.bookNow}>Fashion</div>
+        </div>
+        <div className={styles.newTrendsIn}>New trends in 2023</div>
+        <div className={styles.readMoreWrapper}>
+          <div className={styles.bookNow}>Read More</div>
+        </div>
+        <div className={styles.ametMinimMollit}>
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequat sunt nostrud amet.
+        </div>
+      </div>
+      <div className={styles.rectangleContainer}>
+        <div className={styles.groupChild7} />
+        <img
+          className={styles.unsplashxCxdiuhs3cIcon}
+          alt=""
+          src="/unsplashsd9a6nvhsd4@2x.png"
+        />
+        <div className={styles.beautyGroup}>
+          <div className={styles.bookNow}>{`Beauty `}</div>
+          <img className={styles.vectorIcon2} alt="" src="/vector3.svg" />
+          <div className={styles.bookNow}>Fashion</div>
+        </div>
+        <div className={styles.ametMinimMollit1}>
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequat sunt nostrud amet.
+        </div>
+        <div className={styles.skinHydration}>Skin Hydration</div>
+        <div className={styles.readMoreContainer}>
+          <div className={styles.bookNow}>Read More</div>
+        </div>
+      </div>
+      <img className={styles.groupIcon} alt="" src="/group-13.svg" />
+      <div className={styles.visitOurInstagramContainer}>
+        <span>{`VISIT OUR INSTAGRAM `}</span>
+        <span className={styles.span1}>{`->`}</span>
+      </div>
+      <div className={styles.groupDiv}>
+        <div className={styles.groupChild8} />
+        <div className={styles.resourcesParent}>
+          <div className={styles.resources}>Resources</div>
+          <div className={styles.ourAgents}>Our Agents</div>
+          <div className={styles.memberStories}>Member stories</div>
+          <div className={styles.video}>Video</div>
+          <div className={styles.freeTrial}>Free trial</div>
+        </div>
+        <div className={styles.companyParent}>
+          <div className={styles.company}>Company</div>
+          <div className={styles.home1}>Home</div>
+          <div className={styles.contact1}>Contact</div>
+          <div className={styles.catalogue}>Catalogue</div>
+          <div className={styles.about1}>About</div>
+        </div>
+        <div className={styles.getInTouch}>Get in touch</div>
+        <div
+          className={styles.aGoodSkincare}
+        >{`A good skincare routine will reduce visible pores. Good skincare routine will reduce visible pores `}</div>
+        <img
+          className={styles.rectangleIcon}
+          alt=""
+          src="/rectangle-6@2x.png"
+        />
+        <img className={styles.groupChild9} alt="" src="/rectangle-7@2x.png" />
+        <img className={styles.groupChild10} alt="" src="/rectangle-8@2x.png" />
+        <img className={styles.groupChild11} alt="" src="/rectangle-9@2x.png" />
+        <div className={styles.logo1}>
+          <b className={styles.velvetVanity1}>Velvet Vanity</b>
+          <img className={styles.vectorIcon4} alt="" src="/vector4.svg" />
+        </div>
+        <div className={styles.groupChild12} />
+        <img
+          className={styles.eiarrowRightIcon}
+          alt=""
+          src="/eiarrowright.svg"
+        />
+        <div className={styles.enterYourEmail}>Enter your email</div>
+        <div className={styles.skillIconslinkedinParent}>
+          <img
+            className={styles.skillIconslinkedin}
+            alt=""
+            src="/skilliconslinkedin.svg"
+          />
+          <img
+            className={styles.skillIconsinstagram}
+            alt=""
+            src="/skilliconsinstagram.svg"
+          />
+          <img className={styles.vectorIcon5} alt="" src="/vector5.svg" />
+        </div>
+        <img className={styles.lineIcon} alt="" src="/line-5.svg" />
+        <b className={styles.copyright2023Velvet}>
+          Copyright 2023 -Velvet Vanity
+        </b>
+      </div>
+      <img className={styles.vectorIcon6} alt="" src="/vector6.svg" />
+    </div>
+  );
+};
+
+export default Desktop;
